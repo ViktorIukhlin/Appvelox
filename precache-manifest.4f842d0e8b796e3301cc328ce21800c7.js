bf87b0c631248b057bce5673d5e84f6ed6ef0472
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-10-08 00:02:21 +0300

    Updates

---
+++ precache-manifest.4f842d0e8b796e3301cc328ce21800c7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f653b90b877294c571c495e231fa941",
+    "url": "/Appvelox/index.html"
+  },
+  {
+    "revision": "ab01cd02b1bcd3e7b70a",
+    "url": "/Appvelox/static/css/main.b22dbb84.chunk.css"
+  },
+  {
+    "revision": "4bbdbd4059869ee88de4",
+    "url": "/Appvelox/static/js/2.59a2a718.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/Appvelox/static/js/2.59a2a718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab01cd02b1bcd3e7b70a",
+    "url": "/Appvelox/static/js/main.8d286d5b.chunk.js"
+  },
+  {
+    "revision": "0505b12d6786859983c9",
+    "url": "/Appvelox/static/js/runtime-main.0c5174b6.js"
+  },
+  {
+    "revision": "f2dfdb8318ac9e9968df7a837bcc37dc",
+    "url": "/Appvelox/static/media/avatar.f2dfdb83.jpg"
+  },
+  {
+    "revision": "dc59b67bc760b7e1693f4bc0c3e62c1b",
+    "url": "/Appvelox/static/media/harkov.dc59b67b.jpg"
+  },
+  {
+    "revision": "7954a1b7ec75550ee7f83a2e31569260",
+    "url": "/Appvelox/static/media/malushko.7954a1b7.jpg"
+  },
+  {
+    "revision": "1a5a8a1dc9cbe9e053eb731a30024827",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-300.1a5a8a1d.woff"
+  },
+  {
+    "revision": "8bea42b3154cb0bd0359bb939a246406",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-300.8bea42b3.eot"
+  },
+  {
+    "revision": "9267fed64459c7c837ebd87eb3247852",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-300.9267fed6.ttf"
+  },
+  {
+    "revision": "a19f3245ffb90e5713f1e197db4a9e14",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-300.a19f3245.svg"
+  },
+  {
+    "revision": "dc293e7b94f6ea3c9f3774a164066247",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-300.dc293e7b.woff2"
+  },
+  {
+    "revision": "1495627ba8789c7cc6ccf5e14af07bb4",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-500.1495627b.eot"
+  },
+  {
+    "revision": "619dd20deab6d735e6786de50c9e81e3",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-500.619dd20d.ttf"
+  },
+  {
+    "revision": "982f26e29b228fc048cb499617f4b0e7",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-500.982f26e2.woff2"
+  },
+  {
+    "revision": "de3567016e01bdb8f125e83d7bf8be97",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-500.de356701.svg"
+  },
+  {
+    "revision": "e16f616e17b7d8c15cc94ea34e29b5dc",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-500.e16f616e.woff"
+  },
+  {
+    "revision": "21810e1f18ebda942bf6df389e56c51c",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-700.21810e1f.woff"
+  },
+  {
+    "revision": "3400270bb8aac394ff9ea61c4b3c9dce",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-700.3400270b.svg"
+  },
+  {
+    "revision": "654afbb18457adc4680ae00989c792b3",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-700.654afbb1.ttf"
+  },
+  {
+    "revision": "80a4d83049ec5624ee327f6e210220f5",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-700.80a4d830.eot"
+  },
+  {
+    "revision": "b5d6eb5655c4cb7dcb1b44200a57719a",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-700.b5d6eb56.woff2"
+  },
+  {
+    "revision": "1e54305531d0ca55dee035d28ef70b7e",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-regular.1e543055.woff2"
+  },
+  {
+    "revision": "229188ce84a8ce84c4fc47e250fbd567",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-regular.229188ce.ttf"
+  },
+  {
+    "revision": "85ebc28f175dd24abb96d5de900a3d54",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-regular.85ebc28f.svg"
+  },
+  {
+    "revision": "e5b4f61503d99bee36991d582ef3cbda",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-regular.e5b4f615.woff"
+  },
+  {
+    "revision": "fc401e100e6d7b4c6c6615e6b3bcaa4d",
+    "url": "/Appvelox/static/media/rubik-v11-latin_cyrillic-regular.fc401e10.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ViktorIukhlin/Appvelox/precache-manifest.eaecb5ebbd7ba33fa258082312a24127.js"
+  "/Appvelox/precache-manifest.4f842d0e8b796e3301cc328ce21800c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ViktorIukhlin/Appvelox/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Appvelox/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

f1df4bcc201893bef7edc2ddd6f76a0279e9dc8e
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-10-07 23:18:19 +0300

    finished

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import Appointment from './Main/Appointment/Appointment';
 import ElectronicCards from './Main/ElectronicСard/ElectronicCards';
-import style from './Profile.module.css'
-import jsonStore from '../../redux/jsonStore.json'
+import style from './Profile.module.css';
 
 const Profile = (props) => {
-  const componentDidMount = (store) => {
-    fetch('../../redux/jsonStore.json/listOfRecords')
-    .then((result) => {
-  
-      return result.json();
-    }).then((jsonResult) => {
-      console.log(jsonResult);
-    })
-  }
-  componentDidMount(jsonStore);
-
   return (
     <div className={style.container}>
       <Appointment listOfRecords={props.store.getState().listOfRecords.listOfRecords} /> 
